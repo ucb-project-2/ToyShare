3b6fec839d91f06b387e8a2df1d22e7ab559d315
Ben Lambillotte <ben.lambillotte@gmail.com>
2017-11-27 10:29:15 -0800

    Added validation for only image type files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 // *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// api-routes.js - this file offers a set of routes for sending data 
 // *********************************************************************************
 
 // Dependencies
